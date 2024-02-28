286c46f0cfb6e2cde7945f3617eb4376fc94fb44
Rodrigo Grisi <46337889+RodrigoGrisi@users.noreply.github.com>
2024-02-28 20:43:19 -0300

    Feat: Initializing the SASS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+// import { Inter } from "next/font/google";
+// const inter = Inter({ subsets: ["latin"] });
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
     <>
-    <div>
-      <h1> Sujeito Pizza</h1>
-    </div>
+      <div className={styles.container}>
+        <h1> Sujeito Pizza</h1>
+      </div>
     </>
   );
 }
