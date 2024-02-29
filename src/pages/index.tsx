56bed28dbfcc18c20c9b9308b639b0b3ef0d23c3
Rodrigo Grisi <46337889+RodrigoGrisi@users.noreply.github.com>
2024-02-29 19:04:19 -0300

    Feat: Starting login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-// import { Inter } from "next/font/google";
-// const inter = Inter({ subsets: ["latin"] });
+import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>PizzaDEV - Area de login </title>
+      </Head>
       <div className={styles.container}>
         <h1> Sujeito Pizza</h1>
       </div>
