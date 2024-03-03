390a0bbf2565b1719b128eaf431e51f6449f6a0b
Rodrigo Grisi <46337889+RodrigoGrisi@users.noreply.github.com>
2024-03-03 14:21:30 -0300

    Feat: Login page Created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,23 +6,30 @@ import logoImg from "../../public/logo.svg";
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>PizzaDEV - Area de login </title>
       </Head>
+
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="logo Sujeito Pizzaria" />
+
         <div className={styles.login}>
-          <form className="formLogin">
+          <form>
             <Input placeholder="Digite seu email" type="text" />
             <Input placeholder="Digite sua senha" type="password" />
-            <Button 
-            loading={false}
-            type="submit">Acessar</Button>
+            <Button loading={false} type="submit">
+              Acessar
+            </Button>
           </form>
         </div>
+        <Link href="/signup" legacyBehavior>
+          <a className={styles.text}> Não possui uma conta ? Cadastre-se</a>
+        </Link>
       </div>
     </>
   );
